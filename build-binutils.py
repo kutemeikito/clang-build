89645ae3d81d301e8ac20af0b90546c20655b006
Dakkshesh <dakkshesh5@gmail.com>
2021-10-11 19:09:13 +0530

    build-binutils: Pass O3 flag to linker flags too
    Change-Id: I54d0304f04bb1d7864a2e3eb50fa6c91f5f12561
    Signed-off-by: XSans0 <xsansdroid@gmail.com>

--- build-binutils.py
+++ build-binutils.py
@@ -252,7 +252,7 @@ def invoke_configure(binutils_folder, build_folder, install_folder, target,
             f'CXXFLAGS=-flto -O3 -pipe -ffunction-sections -fdata-sections -march={host_arch} -mtune={host_arch}'
         ]
     else:
-        configure += ['CFLAGS=-flto -O3 -pipe -ffunction-sections -fdata-sections', 'CXXFLAGS=-flto -O3 -pipe -ffunction-sections -fdata-sections']
+        configure += ['CFLAGS=-flto -O3 -pipe -ffunction-sections -fdata-sections', 'CXXFLAGS=-flto -O3 -pipe -ffunction-sections -fdata-sections', 'LDFLAGS=-O3']
     # gprofng uses glibc APIs that might not be available on musl
     if utils.libc_is_musl():
         configure += ['--disable-gprofng']
