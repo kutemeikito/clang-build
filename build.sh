6e0e496831d857c162c9061e318d62ec34d63ded
XSans0 <xsansdroid@gmail.com>
2023-01-25 17:58:07 +0700

    Cleanup
    Signed-off-by: XSans0 <xsansdroid@gmail.com>

--- build.sh
+++ build.sh
@@ -10,41 +10,18 @@ err() {
 }
 
 # Environment checker
-if [ -z "$1" ] || [ -z "$GIT_TOKEN" ] || [ -z "$TELEGRAM_TOKEN" ] || [ -z "$TELEGRAM_CHAT" ]; then
-   echo "* Incomplete environment!"
-   exit
+if [ -z "$TELEGRAM_TOKEN" ] || [ -z "$TELEGRAM_CHAT" ] || [ -z "$BRANCH" ] || [ -z "$GIT_TOKEN" ]; then
+    err "* Incomplete environment!"
+    exit
 fi
 
-# Install dependency
-bash ci.sh deps
-
-# Set a directory
-DIR="$(pwd)"
-BUILD_LOG="$DIR/build_log.txt"
-
-# Setup branch
-BRANCH="$1"
-
-# Cancel if clang is already made for today
-# We only run this when running on GitHub Actions
-if [ "${GITHUB_ACTIONS}" ]; then
-    if [ "$BRANCH" == "main" ]; then
-        wget -q https://raw.githubusercontent.com/XSans0/WeebX-Clang/main/16.0.0/build-date.txt -O date.txt 1>/dev/null 2>/dev/null || echo 'nothing' > date.txt
-    elif [ "$BRANCH" == "release/15.x" ]; then
-        wget -q https://raw.githubusercontent.com/XSans0/WeebX-Clang/main/15.0.3/build-date.txt -O date.txt 1>/dev/null 2>/dev/null || echo 'nothing' > date.txt
-    fi
-
-    if [[ "$(cat date.txt)" == "$(TZ=Asia/Jakarta date +"%Y-%m-%d")" ]]; then
-        msg "Clang is already made for today"
-        rm -rf date.txt
-        exit
-    fi
-fi
+# Set a home directory
+HOME_DIR="$(pwd)"
 
 # Telegram Setup
 git clone --depth=1 https://github.com/XSans0/Telegram Telegram
 
-TELEGRAM="$DIR/Telegram/telegram"
+TELEGRAM="$HOME_DIR/Telegram/telegram"
 send_msg() {
   "${TELEGRAM}" -H -D \
       "$(
@@ -64,31 +41,30 @@ send_file() {
 msg "Building LLVM..."
 send_msg "<b>Clang build started on <code>[ $BRANCH ]</code> branch</b>"
 ./build-llvm.py \
-	--clang-vendor "WeebX" \
-	--defines "LLVM_PARALLEL_COMPILE_JOBS=$(nproc) LLVM_PARALLEL_LINK_JOBS=$(nproc) CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3" \
-	--projects "clang;compiler-rt;lld;polly" \
-	--targets "ARM;AArch64;X86" \
-        --no-ccache \
-        --quiet-cmake \
-        --shallow-clone \
-        --branch "${BRANCH}" 2>&1 | tee "${BUILD_LOG}"
+    --branch "$BRANCH" \
+    --clang-vendor "WeebX" \
+    --defines LLVM_PARALLEL_COMPILE_JOBS=$(nproc) LLVM_PARALLEL_LINK_JOBS=$(nproc) CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3 \
+    --no-ccache \
+    --quiet-cmake \
+    --shallow-clone \
+    --targets "ARM;AArch64" 2>&1 | tee "$HOME_DIR/log.txt"
 
 # Check if the final clang binary exists or not.
 for file in install/bin/clang-1*
 do
-  if [ -e "$file" ]
-  then
-    msg "LLVM building successful"
-  else 
-    err "LLVM build failed!"
-    send_file "$BUILD_LOG" "<b>Clang build failed on <code>[ $BRANCH ]</code> branch</b>"
-    exit
-  fi
+    if [ -e "$file" ]
+    then
+        msg "LLVM building successful"
+    else 
+        err "LLVM build failed!"
+        send_file "$HOME_DIR/log.txt" "<b>Clang build failed on <code>[ $BRANCH ]</code> branch</b>"
+        exit
+    fi
 done
 
 # Build binutils
 msg "Building binutils..."
-./build-binutils.py --targets arm aarch64 x86_64
+./build-binutils.py --targets arm aarch64
 
 # Remove unused products
 rm -fr install/include
@@ -121,8 +97,8 @@ TagsDate="$(TZ=Asia/Jakarta date +"%Y%m%d")"
 BuildDate="$(TZ=Asia/Jakarta date +"%Y-%m-%d")"
 BuildHours="$(TZ=Asia/Jakarta date +"%H%M")"
 ZipName="WeebX-Clang-$clang_version-$TagsDate-$BuildHours.tar.gz"
-Tags="WeebX-Clang-$clang_version-$TagsDate-release"
-ClangLink="https://github.com/XSans0/WeebX-Clang/releases/download/${Tags}/${ZipName}"
+Tags="WeebX-Clang-$clang_version-$TagsDate-$BuildHours-release"
+ClangLink="https://github.com/XSans0/WeebX-Clang/releases/download/$Tags/$ZipName"
 
 # Git Config
 git config --global user.name "XSans0"
@@ -139,31 +115,32 @@ pushd install || exit
 tar -czvf ../"$ZipName" .
 popd || exit
 
-# Clone Repo
+# Push
 git clone "https://XSans0:$GIT_TOKEN@github.com/XSans0/WeebX-Clang.git" rel_repo
 pushd rel_repo || exit
-if [ -d "$clang_version" ]; then
-    echo "${ClangLink}" > "$clang_version"/link.txt
-    echo "${BuildDate}" > "$clang_version"/build-date.txt
+if [ -d "$BRANCH" ]; then
+    echo "$ClangLink" > "$BRANCH"/link.txt
+    cp -r ../install/README.md "$BRANCH"
 else
-    mkdir "$clang_version"
-    echo "${ClangLink}" > "$clang_version"/link.txt
-    echo "${BuildDate}" > "$clang_version"/build-date.txt
+    mkdir -p "$BRANCH"
+    echo "$ClangLink" > "$BRANCH"/link.txt
+    cp -r ../install/README.md "$BRANCH"
 fi
 git add .
-git commit -asm "WeebX-Clang-$clang_version: ${TagsDate}"
-git tag "${Tags}" -m "${Tags}"
+git commit -asm "WeebX-Clang-$clang_version: $TagsDate"
+git tag "$Tags" -m "$Tags"
 git push -f origin main
-git push -f origin "${Tags}"
+git push -f origin "$Tags"
 popd || exit
 
+# Upload to github release
 chmod +x github-release
 ./github-release release \
     --security-token "$GIT_TOKEN" \
     --user XSans0 \
     --repo WeebX-Clang \
-    --tag "${Tags}" \
-    --name "${Tags}" \
+    --tag "$Tags" \
+    --name "$Tags" \
     --description "$(cat install/README.md)"
 
 fail="n"
@@ -171,7 +148,7 @@ fail="n"
     --security-token "$GIT_TOKEN" \
     --user XSans0 \
     --repo WeebX-Clang \
-    --tag "${Tags}" \
+    --tag "$Tags" \
     --name "$ZipName" \
     --file "$ZipName" || fail="y"
 
@@ -202,14 +179,14 @@ fi
 
 if [ "$fail" == "y" ];then
     pushd rel_repo || exit
-    git push -d origin "${Tags}"
+    git push -d origin "$Tags"
     git reset --hard HEAD~1
     git push -f origin main
     popd || exit
 fi
 
 # Send message to telegram
-send_file "$BUILD_LOG" "<b>Clang build successful on <code>[ $BRANCH ]</code> branch</b>"
+send_file "$HOME_DIR/log.txt" "<b>Clang build successful on <code>[ $BRANCH ]</code> branch</b>"
 send_msg "
 <b>----------------- Quick Info -----------------</b>
 <b>Build Date : </b>
@@ -222,5 +199,4 @@ send_msg "
 * <a href='$llvm_commit_url'>$llvm_commit_url</a>
 <b>Push Repository : </b>
 * <a href='https://github.com/XSans0/WeebX-Clang.git'>WeebX-Clang</a>
-<b>--------------------------------------------------</b>
-"
+<b>--------------------------------------------------</b>"
